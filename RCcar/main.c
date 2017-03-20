b41afd99d97f649dc32e24b8a6347e82c4f31d7f
Bartosz Tyczyński <babu50396@gmail.com>
2017-03-20 20:46:53 +0100

    Update comit

--- RCcar/main.c
+++ RCcar/main.c
@@ -1,15 +1,73 @@
-#include "stm32f4xx_conf.h"
+#define true 1
+#include "stm32f4xx_tim.h"
 #include "stm32f4xx_gpio.h"
 #include "stm32f4xx_rcc.h"
+#include "stm32f4xx_usart.h"
+#include "misc.h"
+
+
+
+void USART3_IRQHandler(void)
+{
+// sprawdzenie flagi zwiazanej z odebraniem danych przez USART
+if(USART_GetITStatus(USART3, USART_IT_RXNE) != RESET)
+{
+// odebrany bajt znajduje sie w rejestrze USART3->DR
+		while(USART_GetFlagStatus(USART3, USART_FLAG_TXE) == RESET);
+		char a=USART3->DR;
+		if(a=='w')
+		{
+			GPIO_ResetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
+			GPIO_SetBits(GPIOD, GPIO_Pin_12);
+
+		}
+		if(a=='s')
+		{
+			GPIO_ResetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
+			GPIO_SetBits(GPIOD,GPIO_Pin_13);
+		}
+		if(a=='a')
+		{
+			GPIO_ResetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
+			GPIO_SetBits(GPIOD,GPIO_Pin_14);
+		}
+		if(a=='d')
+		{
+			GPIO_ResetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
+			GPIO_SetBits(GPIOD,GPIO_Pin_15);
+		}
+
+		while (USART_GetFlagStatus(USART3, USART_FLAG_TC) == RESET);
+}
+}
+
 
 int main(void)
 {
 	SystemInit();
 
-	/* GPIOD Periph clock enable */
+	// wlaczenie taktowania wybranego portu
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+	// wlaczenie taktowania wybranego uk³adu USART
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
+
+
+
+	// konfiguracja linii Rx i Tx
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+	// ustawienie funkcji alternatywnej dla pinów (USART)
+	GPIO_PinAFConfig(GPIOC, GPIO_PinSource10, GPIO_AF_USART3);
+	GPIO_PinAFConfig(GPIOC, GPIO_PinSource11, GPIO_AF_USART3);
+
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
-	GPIO_InitTypeDef  GPIO_InitStructure;
+	//zadanie3
 	/* Configure PD12, PD13, PD14 and PD15 in output pushpull mode */
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
@@ -18,14 +76,50 @@ int main(void)
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_Init(GPIOD, &GPIO_InitStructure);
 
-	unsigned int i;
 
-	for(;;)
+	USART_InitTypeDef USART_InitStructure;
+
+	// predkosc transmisji (mozliwe standardowe opcje: 9600, 19200, 38400, 57600, 115200, ...)
+	USART_InitStructure.USART_BaudRate = 9600;
+	// d³ugoœæ s³owa (USART_WordLength_8b lub USART_WordLength_9b)
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	// liczba bitów stopu (USART_StopBits_1, USART_StopBits_0_5, USART_StopBits_2, USART_StopBits_1_5)
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	// sprawdzanie parzystoœci (USART_Parity_No, USART_Parity_Even, USART_Parity_Odd)
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	// sprzêtowa kontrola przep³ywu (USART_HardwareFlowControl_None, USART_HardwareFlowControl_RTS, USART_HardwareFlowControl_CTS, USART_HardwareFlowControl_RTS_CTS)
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	// tryb nadawania/odbierania (USART_Mode_Rx, USART_Mode_Rx )
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+
+	// konfiguracja
+	USART_Init(USART3, &USART_InitStructure);
+
+	// wlaczenie ukladu USART
+	USART_Cmd(USART3, ENABLE);
+
+
+	//zadanie2
+	//struktura do konfiguracji kontrolera NVIC
+	NVIC_InitTypeDef NVIC_InitStructure;
+	// wlaczenie przerwania zwi¹zanego z odebraniem danych (pozostale zrodla przerwan zdefiniowane sa w pliku stm32f4xx_usart.h)
+	USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);
+	NVIC_InitStructure.NVIC_IRQChannel = USART3_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	// konfiguracja kontrolera przerwan
+	NVIC_Init(&NVIC_InitStructure);
+	// wlaczenie przerwan od ukladu USART
+	NVIC_EnableIRQ(USART3_IRQn);
+
+
+	while(1)
 	{
-		GPIO_SetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
-		for (i=0;i<1000000;i++);
-		GPIO_ResetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
-		for (i=0;i<1000000;i++);
+
+
+
 	}
 
 }
+
